fadbf644c5b02dd05ff074e513f26388ef15bac3
alperenkesk <alperenkesk55@gmail.com>
2017-03-21 17:57:05 +0200

    Create yassorgular.py

---
+++ yassorgular.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+yas = input("Yaşınız: ")
+
+if yas < 35:
+
+print("Demek", yas , "yaşındasın.")
+print("Gençmişsin.")
+
+if yas > 36
+
+print("Demek", yas , "yaşındasın.")
+print("Yaşlıymışsın.")
